5ae8dd93626398131270d03b622c2eabe26abab4
Phong <phongit19995@gmail.com>
2020-01-10 20:12:08 +0700

    fix

--- src/controllers/admin/game.js
+++ src/controllers/admin/game.js
@@ -16,21 +16,23 @@ let playGame = async () =>{
         console.log('get info mongo');
     }
     let arrayPromise = usergameconfig.map((item)=>{
-        return runPlayGame(item);
+        return  runPlayGame(item);
     })
-    
     // console.log(usergameconfig); 
-    let result = Promise.all(arrayPromise);
+    let result = await Promise.all(arrayPromise);
     // let resultBaiCao = await playBaiCao(resultLogin);
      
 }
 let runPlayGame = async (userinfo)=>{
     let {iduser,password,numberbaicao,numberlode,baicao,lode} = userinfo ;
+    console.log(iduser,password,numberbaicao,numberlode,baicao,lode);
     let resultLogin = await loginid(iduser,password);
     if(!resultLogin){
         return console.log('Đăng Nhập Không Thành Công :' + iduser);
     }
+    // console.log(resultLogin);
     if (baicao){
+        console.log("Danh bai cao");
         let resultBaiCao = await playBaiCao(resultLogin,numberbaicao);
         console.log(resultBaiCao);
     }
@@ -55,6 +57,7 @@ let loginid = async(id,password)=>{
               }
         } ;
     let result = await request(options);
+    console.log(result);
     return false ;
     } catch (error) {
         if(error.statusCode){
@@ -69,7 +72,8 @@ function randomIntFromInterval(min, max) { // min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 let playBaiCao = async (sessions,moneybaicao)=>{
-    let options = { method: 'POST',
+    let options = {
+         method: 'POST',
     url: URLBAICAO,
     headers: 
      { 'cache-control': 'no-cache',
@@ -90,11 +94,13 @@ let playBaiCao = async (sessions,moneybaicao)=>{
     let result = await request(options);
     let $ = cheerio.load(result);
     let resutBaiCao = $('body > div:nth-child(3) > center').text();
+    // console.log(result);
     return resutBaiCao ;
 }
 let requestlode = async (sessions,moneylode)=>{
     let number = randomIntFromInterval(1,99);
-    let options = { method: 'POST',
+    let options = {
+     method: 'POST',
     url: URLLODE,
     headers: 
      { 'cache-control': 'no-cache',
